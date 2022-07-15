16642c284cf379c576538ddf2d367e786226305d
Neeraj Guleria <neeraj1382@gmail.com>
2022-07-15 22:04:47 +0530

    Buddy Practice

---
+++ cypress/e2e/demo_test.cy.js
@@ -0,0 +1,13 @@
+///<reference types = "cypress"/>
+
+describe('Amazon test',()=> {
+    it('search a keyword on amazon', () =>{
+
+    cy.visit('https://www.amazon.in/');
+    cy.get('#twotabsearchtextbox').type('Books {enter}');
+    //cy.get('#twotabsearchtextbox', {timeout: 7000}).type('Books {enter}');
+    //cy.get('#nav-search-submit-button').click();
+   
+    //cy.contains('Mobiles').click();
+    });
+})
