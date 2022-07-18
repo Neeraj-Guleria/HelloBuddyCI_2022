0bab755c75d15c599af827f9e97b8d9a59647364
Neeraj Guleria <neeraj1382@gmail.com>
2022-07-18 10:49:20 +0530

    Removed the commented part

--- cypress/e2e/demo_test.cy.js
+++ cypress/e2e/demo_test.cy.js
@@ -5,9 +5,9 @@ describe('Amazon test',()=> {
 
     cy.visit('https://www.amazon.in/');
     cy.get('#twotabsearchtextbox').type('Books {enter}');
-    //cy.get('#twotabsearchtextbox', {timeout: 7000}).type('Books {enter}');
-    //cy.get('#nav-search-submit-button').click();
+    cy.get('#twotabsearchtextbox', {timeout: 7000}).type('Books {enter}');
+    cy.get('#nav-search-submit-button').click();
    
-    //cy.contains('Mobiles').click();
+    cy.contains('Mobiles').click();
     });
 })
